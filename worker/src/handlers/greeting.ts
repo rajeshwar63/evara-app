9489dc05df132466ad83d6b3daa8cf5806452512
rajeshwar63 <rajeshwar63@gmail.com>
2026-03-29 09:22:53 +0530

    Delete worker directory

--- worker/src/handlers/greeting.ts
+++
@@ -1,7 +0,0 @@
-import { Env, InboundMessage } from "../types";
-import { sendTextReply } from "../whatsapp";
-import { WELCOME_MESSAGE } from "../prompts";
-
-export async function handleGreeting(env: Env, message: InboundMessage): Promise<void> {
-  await sendTextReply(env, message.from, WELCOME_MESSAGE);
-}
